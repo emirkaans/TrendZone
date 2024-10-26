32c51b95690f80d005eb7709e5d8aae550fcaeaf
emirkaan <emirkaansaricam@gmail.com>
2024-10-26 20:09:31 +0300

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="flex h-10 items-center justify-between space-x-3 bg-amber-400 px-4 py-2 sm:h-16">
+      <header className="flex h-10 items-center justify-between space-x-3 bg-amber-300 px-4 py-2 sm:h-16">
         <NavLink to="/">
           <h1 className="text-xl sm:text-4xl">TrendZone</h1>
         </NavLink>
