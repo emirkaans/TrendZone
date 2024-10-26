a5b7e2358be1b26a4063d6651f6b06cde094731e
emirkaan <emirkaansaricam@gmail.com>
2024-10-26 20:11:20 +0300

    ProductPage update

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 import ProductDetail from "../components/ProductDetail";
+import ProductDetailReviews from "../components/ProductDetailReviews";
+import { useProductContext } from "../contexts/ProductContext";
 
 function ProductPage() {
+  const [product, setProduct] = useState({});
+  const { getProduct } = useProductContext();
+  const { id } = useParams();
+
+  const getMyProduct = async () => {
+    const data = await getProduct(id);
+
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    getMyProduct();
+  }, []);
+
+  if (!product.id) {
+    return <Spinner />;
+  }
+
   return (
     <>
-      <ProductDetail />
+      <ProductDetail product={product} />
+      <ProductDetailReviews product={product} />
     </>
   );
 }
