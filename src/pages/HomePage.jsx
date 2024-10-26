fb96ae0885fac425b0431ce61c20f0f2728a5234
emirkaan <emirkaansaricam@gmail.com>
2024-10-26 20:11:04 +0300

    HomePage update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HomeHero from "../components/HomeHero";
+import HomeProductList from "../components/HomeProductList";
 
 function HomePage() {
   return (
     <>
       <HomeHero />
+      <HomeProductList />
     </>
   );
 }
